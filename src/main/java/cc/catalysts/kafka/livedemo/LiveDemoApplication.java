2900752ba6cadfa92c2d878c77b39ec7de7d1dbc
David Schneiderbauer <dschneiderbauer@gmail.com>
2019-09-17 18:57:32 +0200

    spring boot kafka setup

---
+++ src/main/java/cc/catalysts/kafka/livedemo/LiveDemoApplication.java
@@ -0,0 +1,13 @@
+package cc.catalysts.kafka.livedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveDemoApplication.class, args);
+    }
+
+}
